9336695c33036d0711fd3a320c154d85bc2fb79c
rebecajuliaa9@gmail.com <rebecajuliaa9@gmail.com>
2021-11-15 15:51:18 -0300

    Plataforma

--- sketch.js
+++ sketch.js
@@ -18,7 +18,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    plataforma = new Ground(150,305,300,170);
+
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -62,6 +63,8 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    plataforma.display();
 }
     
     
